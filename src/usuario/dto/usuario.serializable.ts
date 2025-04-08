e8d3b5a443e2aada769678f2ec474585f519c7e4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-08 18:27:53 -0400

    feat(usuario): added 'isActiva' property to usuario.serializable

--- src/usuario/dto/usuario.serializable.ts
+++ src/usuario/dto/usuario.serializable.ts
@@ -1,16 +1,23 @@
-import { RolEnum } from "../entities/usuario.schema"
+import { RolEnum } from '../entities/usuario.schema';
 
 export class UsuarioSerializable {
-    id: string
-    nombreUsuario: String
-    email: String
-    rol: RolEnum
+  id: string;
+  nombreUsuario: String;
+  email: String;
+  rol: RolEnum;
+  isActiva: boolean;
 
-
-    constructor(id: string, nombreUsuario: String, email: String, rol: RolEnum) {
-        this.id = id
-        this.nombreUsuario = nombreUsuario
-        this.email = email
-        this.rol = rol
-    }
+  constructor(
+    id: string,
+    nombreUsuario: String,
+    email: String,
+    rol: RolEnum,
+    isActiva: boolean,
+  ) {
+    this.id = id;
+    this.nombreUsuario = nombreUsuario;
+    this.email = email;
+    this.rol = rol;
+    this.isActiva = isActiva;
+  }
 }
